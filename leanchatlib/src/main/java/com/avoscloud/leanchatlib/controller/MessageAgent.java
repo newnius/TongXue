76619fe2547b1a561d0da3ba37a5e0ae0791e2c8
Super pan <panchaosi5152.com>
2016-03-21 23:45:02 +0800

    添加打开白板时自动发消息通知

--- leanchatlib/src/main/java/com/avoscloud/leanchatlib/controller/MessageAgent.java
+++ leanchatlib/src/main/java/com/avoscloud/leanchatlib/controller/MessageAgent.java
@@ -15,114 +15,128 @@ import com.avoscloud.leanchatlib.utils.Utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by lzw on 14/11/23.
  */
 public class MessageAgent {
-  private AVIMConversation conversation;
-  private ChatManager chatManager;
-  private SendCallback sendCallback = new SendCallback() {
-    @Override
-    public void onError(AVIMTypedMessage message, Exception e) {
+    private AVIMConversation conversation;
+    private ChatManager chatManager;
+    private SendCallback sendCallback = new SendCallback() {
+        @Override
+        public void onError(AVIMTypedMessage message, Exception e) {
 
-    }
-
-    @Override
-    public void onSuccess(AVIMTypedMessage message) {
-
-    }
-  };
-
-  public MessageAgent(AVIMConversation conversation) {
-    this.conversation = conversation;
-    chatManager = ChatManager.getInstance();
-  }
-
-  public void setSendCallback(SendCallback sendCallback) {
-    this.sendCallback = sendCallback;
-  }
-
-  private void sendMsg(final AVIMTypedMessage msg, final String originPath, final SendCallback callback) {
-    if (!chatManager.isConnect()) {
-      Utils.log("im not connect");
-    }
-    conversation.sendMessage(msg, AVIMConversation.RECEIPT_MESSAGE_FLAG, new AVIMConversationCallback() {
-      @Override
-      public void done(AVException e) {
-        if (e == null && originPath != null) {
-          File tmpFile = new File(originPath);
-          File newFile = new File(PathUtils.getChatFilePath(msg.getMessageId()));
-          boolean result = tmpFile.renameTo(newFile);
-          if (!result) {
-            throw new IllegalStateException("move file failed, can't use local cache");
-          }
         }
-        if (callback != null) {
-          if (e != null) {
-            callback.onError(msg, e);
-          } else {
-            RoomsTable.getCurrentUserInstance().insertRoom(conversation.getConversationId());
-            ChatManager.getInstance().insertLatestMessage(msg);
-            callback.onSuccess(msg);
-          }
+
+        @Override
+        public void onSuccess(AVIMTypedMessage message) {
+
         }
-      }
-    });
-  }
+    };
 
-  public void resendMessage(final AVIMTypedMessage msg, final SendCallback sendCallback) {
-    conversation.sendMessage(msg, AVIMConversation.RECEIPT_MESSAGE_FLAG, new AVIMConversationCallback() {
-      @Override
-      public void done(AVException e) {
-        if (e != null) {
-          sendCallback.onError(msg, e);
-        } else {
-          sendCallback.onSuccess(msg);
+    public MessageAgent(AVIMConversation conversation) {
+        this.conversation = conversation;
+        chatManager = ChatManager.getInstance();
+    }
+
+    public void setSendCallback(SendCallback sendCallback) {
+        this.sendCallback = sendCallback;
+    }
+
+    private void sendMsg(final AVIMTypedMessage msg, final String originPath, final SendCallback callback) {
+        if (!chatManager.isConnect()) {
+            Utils.log("im not connect");
         }
-      }
-    });
-  }
-
-  public void sendText(String content) {
-    AVIMTextMessage textMsg = new AVIMTextMessage();
-    textMsg.setText(content);
-    sendMsg(textMsg, null, sendCallback);
-  }
-
-  public void sendImage(String imagePath) {
-    final String newPath = PathUtils.getChatFilePath(Utils.uuid());
-    PhotoUtils.compressImage(imagePath, newPath);
-    try {
-      AVIMImageMessage imageMsg = new AVIMImageMessage(newPath);
-      sendMsg(imageMsg, newPath, sendCallback);
-    } catch (IOException e) {
-      e.printStackTrace();
+        conversation.sendMessage(msg, AVIMConversation.RECEIPT_MESSAGE_FLAG, new AVIMConversationCallback() {
+            @Override
+            public void done(AVException e) {
+                if (e == null && originPath != null) {
+                    File tmpFile = new File(originPath);
+                    File newFile = new File(PathUtils.getChatFilePath(msg.getMessageId()));
+                    boolean result = tmpFile.renameTo(newFile);
+                    if (!result) {
+                        throw new IllegalStateException("move file failed, can't use local cache");
+                    }
+                }
+                if (callback != null) {
+                    if (e != null) {
+                        callback.onError(msg, e);
+                    } else {
+                        RoomsTable.getCurrentUserInstance().insertRoom(conversation.getConversationId());
+                        ChatManager.getInstance().insertLatestMessage(msg);
+                        callback.onSuccess(msg);
+                    }
+                }
+            }
+        });
     }
-  }
 
-  public void sendLocation(double latitude, double longitude, String address) {
-    AVIMLocationMessage locationMsg = new AVIMLocationMessage();
-    AVGeoPoint geoPoint = new AVGeoPoint(latitude, longitude);
-    locationMsg.setLocation(geoPoint);
-    locationMsg.setText(address);
-    sendMsg(locationMsg, null, sendCallback);
-  }
-
-  public void sendAudio(String audioPath) {
-    try {
-      AVIMAudioMessage audioMsg = new AVIMAudioMessage(audioPath);
-      sendMsg(audioMsg, audioPath, sendCallback);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public void resendMessage(final AVIMTypedMessage msg, final SendCallback sendCallback) {
+        conversation.sendMessage(msg, AVIMConversation.RECEIPT_MESSAGE_FLAG, new AVIMConversationCallback() {
+            @Override
+            public void done(AVException e) {
+                if (e != null) {
+                    sendCallback.onError(msg, e);
+                } else {
+                    sendCallback.onSuccess(msg);
+                }
+            }
+        });
     }
-  }
 
-  public interface SendCallback {
+    public void sendText(String content) {
+        AVIMTextMessage textMsg = new AVIMTextMessage();
+        textMsg.setText(content);
+        sendMsg(textMsg, null, sendCallback);
+    }
 
-    void onError(AVIMTypedMessage message, Exception e);
+    public void sendBoardText(String content){
+        //为区分消息，添加消息类型
+        Map<String,Object> map = new HashMap<>();
+        map.put("type","board");
 
-    void onSuccess(AVIMTypedMessage message);
+        AVIMTextMessage textMsg = new AVIMTextMessage();
+        textMsg.setAttrs(map);
+        textMsg.setText(content);
+        sendMsg(textMsg, null, sendCallback);
+    }
 
-  }
+    public void sendImage(String imagePath) {
+        final String newPath = PathUtils.getChatFilePath(Utils.uuid());
+        PhotoUtils.compressImage(imagePath, newPath);
+        try {
+            AVIMImageMessage imageMsg = new AVIMImageMessage(newPath);
+            sendMsg(imageMsg, newPath, sendCallback);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendLocation(double latitude, double longitude, String address) {
+        AVIMLocationMessage locationMsg = new AVIMLocationMessage();
+        AVGeoPoint geoPoint = new AVGeoPoint(latitude, longitude);
+        locationMsg.setLocation(geoPoint);
+        locationMsg.setText(address);
+        sendMsg(locationMsg, null, sendCallback);
+    }
+
+    public void sendAudio(String audioPath) {
+        try {
+            AVIMAudioMessage audioMsg = new AVIMAudioMessage(audioPath);
+            sendMsg(audioMsg, audioPath, sendCallback);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public interface SendCallback {
+
+        void onError(AVIMTypedMessage message, Exception e);
+
+        void onSuccess(AVIMTypedMessage message);
+
+    }
 }
